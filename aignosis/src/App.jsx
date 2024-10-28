ffd48cf843799c6d41e12a0baa5a85c72ecf34db
harshitdoggleindia <harshit.sharma@doggleindia.com>
2024-10-28 07:22:45 +0000

    all error are resolve

--- aignosis/src/App.jsx
+++ aignosis/src/App.jsx
@@ -4,6 +4,8 @@ import Laptop from './components/Laptop'
 import Screen3 from './components/Screen3'
 import Screen4 from './components/Screen4'
 import Screen5 from './components/Screen5'
+import Component1 from './components/AutismTest'
+import MainComponent from './components/MainComponent'
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <Screen3 />
       <Screen4 />
       <Screen5 />
+      <MainComponent />
     </div>
   )
 }
