e65ea81dd181cbe82f68d98713e33cbeb3afe481
harshitdoggleindia <harshit.sharma@doggleindia.com>
2024-10-29 13:05:05 +0000

    patient history form done

--- aignosis/src/App.jsx
+++ aignosis/src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Im
 import HomepageMain from './components/HomepageMain'; // Import the HomepageMain component
 import AboutUs from './components/AboutUs';
 import PartnershipPage from './components/PartnershipPage';
+import PatientHistoryForm1 from './components/patient-history-form/PatientHistoryForm1';
+import PatientHistoryForm2 from './components/patient-history-form/PatientHistoryForm2';
+import PatientHistoryForm3 from './components/patient-history-form/PatientHistoryForm3';
+import PatientHistoryForm4 from './components/patient-history-form/PatientHistoryForm4';
 
 const App = () => {
   return (
@@ -11,7 +15,10 @@ const App = () => {
         <Route path="/" element={<HomepageMain />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/partnership" element={<PartnershipPage />} />
-
+        <Route path="/form1" element={<PatientHistoryForm1 />} />
+        <Route path="/form2" element={<PatientHistoryForm2 />} />
+        <Route path="/form3" element={<PatientHistoryForm3 />} />
+        <Route path="/form4" element={<PatientHistoryForm4 />} />
       </Routes>
     </Router>
   );
