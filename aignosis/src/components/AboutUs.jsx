15acfd5a5ba87ddf1e2ed004595535a94ec6f6de
Khushi Kumar <khushi.kumar@doggleindia.com>
2024-11-06 07:54:26 +0000

    about us modified

--- aignosis/src/components/AboutUs.jsx
+++ aignosis/src/components/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import img from '../assets/images/SectionL.png';
 import { Link } from 'react-router-dom';
 import Header from './Header';
-import image1 from '../assets/members/image1.png'
+import image1 from '../assets/members/image1.jpg'
 import image2 from '../assets/members/image2.png'
 import image3 from '../assets/members/image3.png'
 import image4 from '../assets/members/image4.png'
@@ -10,8 +10,9 @@ import image5 from '../assets/members/image5.png'
 import image6 from '../assets/members/image6.png'
 import image7 from '../assets/members/image7.png'
 import image9 from '../assets/members/image9.png'
+import about from '../assets/aboutus.png'
 
-import archit_img from '../assets/members/archit_img.jpeg'
+import archit_img from '../assets/members/archit_img.jpg'
 import Partner from './Partner';
 
 const Divider = ({ title }) => (
@@ -51,12 +52,12 @@ const AboutUs = () => {
                         </h1>
                         <p className="mt-3 text-sm sm:text-base">Working together for a brighter future.</p>
                     </div>
-                    <div className="w-[90%] md:w-[40vw] mt-[2vw] h-[40vw] md:h-[20vw] bg-[#D9D9D9]"></div>
-                    <p className="w-[90%] md:w-[60vw] text-center mt-[2vw] text-xs sm:text-sm md:text-base">
-                        At Ai.gnosis, we are committed to making a positive impact on the lives of children and families 
-                        through early autism screening and support. Our team harnesses the power of advanced AI to enable early, 
-                        accessible diagnoses and provide parents with the resources they need for timely intervention. With a 
-                        focus on compassionate care and innovation, we’re dedicated to guiding families on the path to better 
+                    <img src={about} alt="About Us" className="w-[90%] md:w-[40vw] h-[40vw] md:h-[20vw] object-cover mt-[2vw]" />
+                    <p className="w-[90%] md:w-[60vw] text-center mt-[2vw] text-xxxs sm:text-xxs md:text-base">
+                        At Ai.gnosis, we are committed to making a positive impact on the lives of children and families
+                        through early autism screening and support. Our team harnesses the power of advanced AI to enable early,
+                        accessible diagnoses and provide parents with the resources they need for timely intervention. With a
+                        focus on compassionate care and innovation, we’re dedicated to guiding families on the path to better
                         understanding and support.
                     </p>
                 </section>
@@ -79,20 +80,21 @@ const AboutUs = () => {
                     </div>
 
                     <div className="flex flex-wrap justify-center items-center gap-5 md:gap-[7vw]">
-                        <TeamMember name="Dr. S. Sitaraman" img={image1} description="DIRECTOR & DEVELOPMENTAL NEUROLOGIST" height="h-[6rem]" /> 
+                        <TeamMember name="Dr. S. Sitaraman" img={image1} description="DIRECTOR & DEVELOPMENTAL NEUROLOGIST" height="h-[6rem]" />
                         <TeamMember name="Dr. Ramesh Agrawal" img={image6} description="HOD mother & child block, AIIMS Delhi" height="h-[6rem]" />
                         <TeamMember name="Dr. Rajesh Jain" img={image4} description="Consultant Pediatrician Raksheet Hospital" height="h-[6rem]" />
                     </div>
-                  
-                    <div className="flex flex-wrap justify-center items-center gap-5 md:gap-[5vw] mt-10 mb-5">
+
+                    <div className="flex flex-wrap justify-center items-center gap-5 md:gap-[5vw] mt-10 mb-5 pb-5 ">
                         <TeamMember name="Dr. Dhananajay Mangal" img={image5} description="Consultant Pediatrician Babylon Hospital" height="h-[6rem]" />
                         <TeamMember name="Dr. Anami Gour" img={image7} description="Consultant Paediatric Intensive Care, London" height="h-[6rem]" />
                         <TeamMember name="Mr. Rajneesh Bhandari" img={image9} description="NeuroEquilibrium Diagnostics Pvt. LMT." height="h-[6rem]" />
                         <TeamMember name="Mr. Arcchit Jain" img={archit_img} description="Vice President, Goldman Sachs" height="h-[6rem]" />
                     </div>
+
                 </div>
             </section>
-            
+
             <div className='mt-[3vw]'>
                 <Partner />
             </div>
@@ -105,7 +107,7 @@ const AboutUs = () => {
                     <p className="mt-8 text-sm sm:text-base md:text-lg">
                         We’d love to hear from you! Whether you have questions, feedback, or would like to schedule a visit, our contact form makes it easy to reach out.
                     </p>
-                    
+
                     <div className="flex flex-wrap mt-10 gap-5">
                         <div className="w-full md:w-[45%] h-[40vw] md:h-[30vw]">
                             <iframe
