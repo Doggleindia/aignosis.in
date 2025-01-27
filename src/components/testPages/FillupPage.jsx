c4a9de338f40b52e6b20c794ffbf64d9754fa119
cryptbird <khushvardhanbhardwaj@gmail.com>
2025-01-27 12:51:26 +0530

    fillUp page height fix

--- src/components/testPages/FillupPage.jsx
+++ src/components/testPages/FillupPage.jsx
@@ -147,7 +147,7 @@ export const FillupPage = () => {
   
   return (
     <>
-      <div className="bg-[#1A0C25] flex flex-col justify-center items-center min-h-screen">
+      <div className="bg-[#1A0C25] flex flex-col justify-center items-center h-[110vh] ">
         {/* <StepProgress /> */}
         {!isBackInfoVisible ? (
           <div className="flex flex-row  max-sm:flex-col max-sm:justify-center items-center justify-between mt-[10px] max-sm:mt-0 max-sm:mb-[50px]">
