263efbee5d4a51e3433b90c3ec588b82a6cb1036
aaryadevchandra <aaryadevc@gmail.com>
2025-04-16 16:38:32 +0530

    added: sending video language in formdata

--- src/components/testPages/VideoPlayback.jsx
+++ src/components/testPages/VideoPlayback.jsx
@@ -206,7 +206,7 @@ const VideoPlayback = () => {
       console.log("Uploading with FPS:", fps);
       
       formData.append("fps", fps.toString()); // Convert fps to string
-      formData.append("video_language", testData.videolanguage);
+      formData.append("videolanguage", testData.videolanguage);
       
       for (let pair of formData.entries()) {
         console.log(pair[0] + ': ' + pair[1]);
