00633b3df94e2974375e2a0b9dbf55d1d419588f
aaryadevchandra <aaryadevc@gmail.com>
2025-04-25 17:13:12 +0530

    added: cleaning user phone number for uid

--- src/components/testPages/FillupPage.jsx
+++ src/components/testPages/FillupPage.jsx
@@ -253,7 +253,7 @@ export const FillupPage = () => {
 
       setTestData({
         ...testData,
-        PATIENT_UID: JSON.parse(localStorage.getItem("user")).phoneNumber.toString(),
+        PATIENT_UID: cleanedPhoneNumber,
         TRANSACTION_ID: uuidv4(),
       });
     } else {
