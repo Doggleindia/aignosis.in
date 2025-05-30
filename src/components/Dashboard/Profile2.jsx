ccee54821d52ff145ab0896e2290127a3302dda1
mridxl <mridxl03@gmail.com>
2025-05-30 10:39:21 +0530

    fix: update API base URL from prod to de in Profile2 component

--- src/components/Dashboard/Profile2.jsx
+++ src/components/Dashboard/Profile2.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import baby from './baby.png';
 import { decryptPassword, decryptCalibrationData } from '../aignosisintegration/DecryptionUtils';
 
-const API_BASE_URL = 'https://prod.aignosismdw.in/rest/';
+const API_BASE_URL = 'https://de.aignosismdw.in/rest/';
 
 const Profile2 = () => {
   const userId = JSON.parse(localStorage.getItem('user'));
@@ -42,7 +42,7 @@ const Profile2 = () => {
     try {
       const response = await axios
         .post(
-          'https://prod.aignosismdw.in/rest/get_transactions/',
+          'https://de.aignosismdw.in/rest/get_transactions/',
           {
             patient_uid: userId.phoneNumber.toString(),
           },
